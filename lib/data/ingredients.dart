509ff366ac6c40c5c365f2994bff70059252f40d
chisom promise <chimisepro@gmail.com>
2024-02-06 10:37:36 +0100

    feat: create ingredient model and dummy data

---
+++ lib/data/ingredients.dart
@@ -0,0 +1,8 @@
+import 'package:foodrecipe/models/ingredient.dart';
+export 'ingredients.dart' show dummyIngredients;
+
+const List<Ingredient> dummyIngredients = [
+  Ingredient('Breads', 'assets/ingredient-1.png', '200g'),
+  Ingredient('Eggs', 'assets/ingredient-2.png', '200g'),
+  Ingredient('Milk', 'assets/ingredient-1.png', '200g')
+];
---
+++ lib/models/ingredient.dart
@@ -0,0 +1,7 @@
+class Ingredient {
+  final String name;
+  final String image;
+  final String quantity;
+
+  const Ingredient(this.name, this.image, this.quantity);
+}
